55d7e155444f0209b97843d8b12dcfe133fb58de
Daniel Antunes de Almeida <62892836+dan-dealmeida@users.noreply.github.com>
2023-10-02 15:44:30 -0700

    Update RecuperarSenha.js
    Implemente verificação se o e-mail possui um formato válido antes de prosseguir com a lógica de recuperação de senha.

--- RecuperarSenha.js
+++ RecuperarSenha.js
@@ -1,71 +1,67 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
-import Botao from "../Components/Botao";
+import Botao from "./Components/Botao";
 import { useState } from "react";
 
-const RecuperarSenha = (props) =>{
-
-    const [txtEmail, setEmail] = useState('')
-    const [txtError, setError] = useState('')
+const RecuperarSenha = (props) => {
+    const [txtEmail, setEmail] = useState('');
+    const [txtError, setError] = useState('');
 
     const validar = () => {
-        if(txtEmail != 0){
-            setError('E-mail invalido')
+        // Expressão regular para verificar o formato do e-mail
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(txtEmail)) {
+            setError('E-mail inválido');
+        } else {
+            setError('');
+            // Lógica para enviar o e-mail de recuperação de senha
         }
-        else{
-            setError("")
-        }
-        
     }
 
-    return(
+    return (
         <View style={estilo.tela}>
             <View style={estilo.cImput}>
-                <Text style={estilo.Texto} >E-mail: </Text>
-                <TextInput style={estilo.escrita}  value={txtEmail} onChangeText={setEmail}/>
+                <Text style={estilo.Texto}>E-mail:</Text>
+                <TextInput style={estilo.escrita} value={txtEmail} onChangeText={setEmail} />
                 <Text style={estilo.erroou}>{txtError}</Text>
             </View>
 
             <View style={estilo.cBotao}>
-                <Botao texto="Recuperar" funcao={validar}/>
+                <Botao texto="Recuperar" funcao={validar} />
             </View>
-
         </View>
-
     )
 }
 
 const estilo = StyleSheet.create({
-    erroou:{
+    erroou: {
         fontSize: 20,
         color: 'red'
     },
-    Texto:{
+    Texto: {
         fontFamily: 'AveriaLibre-Regular',
         fontSize: 24,
         color: '#ffffff'
     },
-    tela:{
+    tela: {
         backgroundColor: '#372775',
         padding: 35,
         flex: 1
-        
     },
-    escrita:{
+    escrita: {
         backgroundColor: '#ffffff',
         fontFamily: 'AveriaLibre-Regular',
         fontSize: 24,
         color: '#3F92C5'
     },
-    cImput:{
+    cImput: {
         flex: 0.5,
         justifyContent: "flex-end"
-
-        
     },
-    cBotao:{
+    cBotao: {
         flex: 0.25,
         justifyContent: "center"
     }
 })
 
-export default RecuperarSenha
+export default RecuperarSenha;
